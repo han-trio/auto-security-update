68f081d7c9c3c9f14ef6b347c4a38efa01a48271
han-trio <49106396+han-trio@users.noreply.github.com>
2022-04-05 12:16:44 +0200

    Add files via upload

---
+++ auto-security-update.sh
@@ -0,0 +1,81 @@
+#/bin/bash
+
+# script to look for security updates, apply them automatically, and then check if reboot is needed
+# is meant to run as cronjob
+# CentOS specific!
+
+# prerequisites
+
+# make sure log file is present
+touch /var/log/yum-auto-updates.log
+
+# vars
+DATE=$(date +%F\ %T)
+LOG="/var/log/yum-auto-updates.log"
+CHECK=$(which needs-restarting)
+SHUTDOWN=$(which shutdown)
+SYSTEMCTL=$(which systemctl)
+REBOOT=$(which reboot)
+REBOOT_VAR="255"
+
+echo -e "\n$DATE" >> $LOG
+
+# install "needs-restarting" binary, if becessary
+[[ -z $(yum list installed | grep "yum-utils") ]] && yum install yum-utils && echo -e "yum-utils need to be installed..\tdone" >> $LOG
+
+#####
+
+# update package list (check for updates of ALREADY INSTALLED packages)
+# yum check-update
+# Probably unnecessary, because "yum upgrade" does this anyway.
+
+# alt:
+yum makecache && echo -e "cache has been updated" >> $LOG
+# see: https://unix.stackexchange.com/questions/6252/what-is-yum-equivalent-of-apt-get-update
+
+#####
+
+# SECURITY upgrades only
+# see: https://access.redhat.com/solutions/10021
+
+# install packages that have a security errata issue:
+yum update-minimal --security -y
+echo -e "Security updates have been checked @ $(date +%T).\nPlease find more infos in local yum.log\n" >> $LOG
+        # THIS :)
+
+#####
+
+# check for services that need reboot
+# needs-restarting -s
+
+for service in $($CHECK -s); do
+        $SYSTEMCTL restart $service && echo -e "$service\tneeded restart. Done." >> $LOG
+done
+
+#####
+
+# check if general reboot is needed, using:
+# needs-restarting -r
+
+# exit 0 = NO reboot required
+# exit 1 = reboot required
+
+$CHECK -r
+REBOOT_VAR=$?
+
+case $REBOOT_VAR in
+        0)
+        echo -e "$(date +%T)\tNo general reboot required. Resuming normal operation." >> $LOG
+        ;;
+
+        1)
+        echo -e "$(date +%T)\tGeneral reboot required, doing so in 1 min.." >> $LOG
+        wall "Server reboot in 1 minute! SAVE YOUR WORK!!"
+        # $SHUTDOWN -r
+        $REBOOT
+        ;;
+
+        *)
+        echo -e "$(date +%T)\tSomething went wrong with the reboot necessity check - please evaluate." >> $LOG
+        ;;
+esac
